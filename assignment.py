5619a8821b9733d430854d32b2aeecbc6d9666a6
blessymuigai2023-gif <blessymuigai2023@gmail.com>
2026-03-13 14:11:30 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,29 @@
 
+# QUESTION 5
+Name= "Blessy"
+Age= 23
+City= "Nairobi"
+print("Name:",Name)
+#Name: Blessy
+print("Age:",Age)
+#Age: 23
+print("City:",City)
+#City: Nairobi
+
+# QUESTION 6
+languages= ["Python","Java","C++","JavaScript","Go"]
+print(languages[0])
+#Output- python
+
+# QUESTION 7
+student= {
+  "Name":"Blessy",
+  "Age":23,
+  "Course":"AI"
+}
+print(student["Name"])
+#Output- Blessy
+
+# Bonus question
+# Difference between a list and a Key.
+# A list is an ordered collection of elements accessed by their integer index, while a dictionary is a collection of key-value pairs accessed by unique, meaningful keys
